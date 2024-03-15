2a02684e5ffbab1c264128fbc3307d7911664785
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-03-14 23:29:41 -0300

    add vue practical activity

---
+++ Arquitetura de Front End/Prática/atv-pratica-01-vue/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/blog">Blog</RouterLink>
+      <RouterLink to="/">About</RouterLink>
+      <RouterLink to="/description">Description</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+main {
+  margin-top: var(--header-height);
+}
+
+nav {
+  position: fixed;
+  inset: 0 0 auto 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  gap: 16px;
+  height: var(--header-height);
+
+  background-color: var(--primary-color);
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+  font-weight: 500;
+}
+</style>
---
+++ Arquitetura de Front End/Prática/atv-pratica-01-vue/src/components/AboutContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="about">
+    <img alt="Dog" src="@/assets/dog.png" />
+    <section class="info">
+      <h1>Ítalo Médici</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 150px;
+  aspect-ratio: 1 / 1;
+  border-radius: 100%;
+  object-fit: cover;
+}
+.about {
+  display: grid;
+  grid-template-columns: 150px 0.5fr;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+  width: 100%;
+  padding: 48px 24px;
+  background-color: var(--primary-color);
+}
+
+h1 {
+  font-size: 48px;
+  font-weight: 600;
+  margin-bottom: 14px;
+  color: #fff;
+}
+
+p {
+  line-height: 1.5;
+  color: #fff;
+  opacity: 0.8;
+  font-weight: 400;
+}
+
+@media (max-width: 768px) {
+  .about {
+    grid-template-columns: 1fr;
+    justify-items: center;
+    gap: 24px;
+  }
+}
+</style>
